576ebecde065c6141b731d6bbbbeee78b8ca37b5
Kirim Lee <uthi1004@gmail.com>
2021-01-05 19:01:02 +0900

    tailwind css setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import 'tailwindcss/tailwind.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="bg-gray-100 rounded-xl p-8">
+      <header className="App-header bg-yellow-100">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
