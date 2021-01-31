02d7e93f5ab14cbbbf13e18aeb54c1afed8e8c59
Kirim Lee <uthi1004@gmail.com>
2021-01-31 13:49:36 +0900

    restaurant query

---
+++ src/__generated__/restaurantQuery.ts
@@ -0,0 +1,59 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL query operation: restaurantQuery
+// ====================================================
+
+export interface restaurantQuery_allCategories_categories {
+  __typename: "Category";
+  id: number;
+  name: string;
+  icon: string | null;
+  slug: string;
+  restaurantCount: number;
+}
+
+export interface restaurantQuery_allCategories {
+  __typename: "AllCategoriesOutput";
+  ok: boolean;
+  error: string | null;
+  categories: restaurantQuery_allCategories_categories[] | null;
+}
+
+export interface restaurantQuery_restaurants_results_category {
+  __typename: "Category";
+  id: number;
+  name: string;
+  icon: string | null;
+}
+
+export interface restaurantQuery_restaurants_results {
+  __typename: "Restaurant";
+  id: number;
+  name: string;
+  coverImage: string;
+  category: restaurantQuery_restaurants_results_category;
+  address: string;
+  isPromoted: boolean;
+}
+
+export interface restaurantQuery_restaurants {
+  __typename: "RestaurantsOutput";
+  ok: boolean;
+  error: string | null;
+  totalPages: number | null;
+  totalResults: number | null;
+  results: restaurantQuery_restaurants_results[] | null;
+}
+
+export interface restaurantQuery {
+  allCategories: restaurantQuery_allCategories;
+  restaurants: restaurantQuery_restaurants;
+}
+
+export interface restaurantQueryVariables {
+  page?: number | null;
+}
--- src/pages/client/restaurant.tsx
+++ src/pages/client/restaurant.tsx
@@ -1,4 +1,52 @@
+import { gql, useQuery } from '@apollo/client';
+import {
+  restaurantQuery,
+  restaurantQueryVariables,
+} from '../../__generated__/restaurantQuery';
+
+const RESTAURANT_QUERY = gql`
+  query restaurantQuery($page: Float) {
+    allCategories {
+      ok
+      error
+      categories {
+        id
+        name
+        icon
+        slug
+        restaurantCount
+      }
+    }
+
+    restaurants(page: $page) {
+      ok
+      error
+      totalPages
+      totalResults
+      results {
+        id
+        name
+        coverImage
+        category {
+          id
+          name
+          icon
+        }
+        address
+        isPromoted
+      }
+    }
+  }
+`;
 export const Restaurants = () => {
+  const { data, loading, error } = useQuery<
+    restaurantQuery,
+    restaurantQueryVariables
+  >(RESTAURANT_QUERY, { variables: { page: 1 } });
+
+  console.log(data?.allCategories);
+  console.log(data?.restaurants);
+
   return (
     <h1>
       uber its header -<p>category</p>
