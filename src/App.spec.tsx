345ea2d8cd59e1f85746c1852f7d95447a9b1bce
Kirim Lee <uthi1004@gmail.com>
2021-02-03 18:02:13 +0900

    add test app.tsx

---
+++ src/App.spec.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { render, waitFor } from '@testing-library/react';
+import App from './App';
+import { isLoggedInVar } from './apollo';
+
+jest.mock('./routers/logged-out-router', () => ({
+  LoggedOutRouter: () => <span>logged-out</span>,
+}));
+
+jest.mock('./routers/logged-in-router', () => ({
+  LoggedInRouter: () => <span>logged-in</span>,
+}));
+
+describe('<App />', () => {
+  it('render LoggedOutRouter', () => {
+    const { debug, getByText } = render(<App />);
+    getByText('logged-out');
+    debug();
+  });
+
+  it('render LoggedInRouter', async () => {
+    const { debug, getByText } = render(<App />);
+    await waitFor(() => {
+      isLoggedInVar(true);
+    });
+    getByText('logged-in');
+    debug();
+  });
+});
--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,10 @@ import { LoggedInRouter } from './routers/logged-in-router';
 import { LoggedOutRouter } from './routers/logged-out-router';
 import { isLoggedInVar } from './apollo';
 
-function App() {
+const App = () => {
   const isLoggedIn = useReactiveVar(isLoggedInVar);
 
   return isLoggedIn ? <LoggedInRouter /> : <LoggedOutRouter />;
-}
+};
 
 export default App;
